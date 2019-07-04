75166a8d265190d06ad39e6deb65864f6a00dd6e
Phước Trung <trungkenbi@hotmail.com>
2019-07-04 14:41:49 +0700

    Start

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function(io) {
     var app = require('express');
     var router = app.Router();
     io.on('connection', function(socket) {
-        console.log('User connection !!!');
+        console.log('User connection');
         socket.on('disconnect', function () {
            console.log('User disconnection !');
         });
